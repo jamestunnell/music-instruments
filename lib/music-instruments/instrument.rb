4c58fdd326c4f84df80723460e1c34c77ecd6ace
James Tunnell <jamestunnell@lavabit.com>
2013-08-26 13:53:26 -0700

    Add instrument presets, and methods: apply_setting, apply_settings, apply_preset, apply_presets, and configure.

--- lib/music-instruments/instrument.rb
+++ lib/music-instruments/instrument.rb
@@ -11,15 +11,41 @@ class Instrument
     :sample_rate => arg_spec(:reqd => true, :type => Fixnum, :validator => ->(a){ a > 0 }),
     :key_maker => arg_spec(:reqd => true, :type => Proc, :validator => ->(a){ a.arity == 0 }),
     :parameters => arg_spec_hash(:reqd => false, :type => Parameter),
+    :presets => arg_spec_hash(:reqd => false, :type => Hash)
   }
   
-  attr_reader :keys, :sample_rate, :parameters
+  attr_reader :keys, :sample_rate, :parameters, :presets
   
   def initialize args
     hash_make args, Instrument::ARG_SPECS
     @keys = {}
   end
   
+  def apply_setting param_key, val
+    @parameters[param_key].set_value val
+  end
+
+  def apply_settings settings
+    settings.each do |param_key, val|
+      @parameters[param_key].set_value val
+    end
+  end
+
+  def apply_preset preset_key
+    apply_settings @presets[preset_key]
+  end
+
+  def apply_presets preset_keys
+    preset_keys.each do |preset_key|
+      apply_preset preset_key
+    end
+  end
+
+  def configure presets = [], settings = {}
+    apply_presets presets
+    apply_settings settings
+  end
+
   # Render samples using all of the active Key objbects.
   # @param [Fixnum] count The number of samples to render
   def render count
--- spec/instrument_spec.rb
+++ spec/instrument_spec.rb
@@ -1,4 +1,86 @@
 require 'spec_helper'
 
+class FakeInstr < Instrument
+  def initialize params = {}, presets = {}
+    super(
+      :sample_rate => 1000,
+      :key_maker => ->(){},
+      :parameters => params,
+      :presets => presets
+    )
+  end
+end
+
 describe Instrument do
+  describe '#apply_setting' do
+    it 'should call set_value on the given param' do
+      a = 1
+      i = FakeInstr.new( { :a => Parameter.new(->(){ a }, ->(b){ a = b}) } )
+      i.apply_setting(:a,2)
+      a.should eq(2)
+    end
+  end
+
+  describe '#apply_setting' do
+    it 'should call apply_setting for each key/val pair in given hash' do
+      a = 1
+      b = 10
+      i = FakeInstr.new({ 
+        :a => Parameter.new(->(){ a }, ->(v){ a = v}),
+        :b => Parameter.new(->(){ b }, ->(v){ b = v}),
+      })
+      i.apply_settings(:a => 2, :b => 11)
+      a.should eq(2)
+      b.should eq(11)
+    end
+  end
+
+  describe '#apply_preset' do
+    it 'should call apply_setting using the key/val pairs from the given preset' do
+      a = 1
+      i = FakeInstr.new( { :a => Parameter.new(->(){ a }, ->(b){ a = b}) }, :default => {:a => 2} )
+      i.apply_preset(:default)
+      a.should eq(2)
+    end
+  end
+
+  describe '#apply_presets' do
+    it 'should call apply_preset for each key/val pair in given hash' do
+      a = 1
+      b = 10
+      i = FakeInstr.new({ 
+        :a => Parameter.new(->(){ a }, ->(v){ a = v}),
+        :b => Parameter.new(->(){ b }, ->(v){ b = v}),
+      }, :a_default => { :a => 2 }, :b_default => { :b => 11 })
+      i.apply_presets([:a_default, :b_default])
+      a.should eq(2)
+      b.should eq(11)
+    end
+  end
+
+  describe '#configure' do
+    it 'should call apply_presets for given preset keys' do
+      a = 1
+      b = 10
+      i = FakeInstr.new({ 
+        :a => Parameter.new(->(){ a }, ->(v){ a = v}),
+        :b => Parameter.new(->(){ b }, ->(v){ b = v}),
+      }, :a_default => { :a => 2 }, :b_default => { :b => 11 })
+      i.configure([:a_default, :b_default])
+      a.should eq(2)
+      b.should eq(11)      
+    end
+
+    it 'should call apply_settings for given settings hash' do
+      a = 1
+      b = 10
+      i = FakeInstr.new({ 
+        :a => Parameter.new(->(){ a }, ->(v){ a = v}),
+        :b => Parameter.new(->(){ b }, ->(v){ b = v}),
+      }, :a_default => { :a => 2 }, :b_default => { :b => 11 })
+      i.configure([], :a => 3, :b => 12)
+      a.should eq(3)
+      b.should eq(12)
+    end
+  end
 end
